8fd76136ce6876bf037ebffd7f1eca814e952ffb
Daniel Smanioto <daniel.smanioto@gmail.com>
2019-04-24 20:16:04 -0300

    Refatorando metodos ordem de venda

--- src/main/java/com/dmanioto/burger/model/OrderSale.java
+++ src/main/java/com/dmanioto/burger/model/OrderSale.java
@@ -12,101 +12,98 @@ import javax.persistence.ManyToMany;
 
 @Entity
 public class OrderSale {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 
-	@ManyToMany(fetch = FetchType.EAGER)
-	private List<OrderItem> itens;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@ManyToMany
-	private List<OrderDiscount> discounts;
-	
-	private BigDecimal totalPrice;
-	
-	public OrderSale() {
-		// Constructor default
-	}
-	
-	public OrderSale(List<OrderItem> itens) {
-		this.itens = itens;
-	}
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<OrderItem> itens;
 
-	public Long getId() {
-		return id;
-	}
+    @ManyToMany
+    private List<OrderDiscount> discounts;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    private BigDecimal totalPrice;
 
-	public List<OrderItem> getItens() {
-		return itens;
-	}
+    public OrderSale() {
+        // Constructor default
+    }
 
-	public void setItens(List<OrderItem> itens) {
-		this.itens = itens;
-	}
+    public OrderSale(List<OrderItem> itens) {
+        this.itens = itens;
+    }
 
-	public List<OrderDiscount> getDiscounts() {
-		return discounts;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setDiscounts(List<OrderDiscount> discounts) {
-		this.discounts = discounts;
-	}
-	
-	public BigDecimal getTotalPrice() {
-		return totalPrice;
-	}
-	
-	public void setTotalPrice(BigDecimal totalPrice) {
-		this.totalPrice = totalPrice;
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((itens == null) ? 0 : itens.hashCode());
-		return result;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		OrderSale other = (OrderSale) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (itens == null) {
-			if (other.itens != null)
-				return false;
-		} else if (!itens.equals(other.itens))
-			return false;
-		return true;
-	}
+    public List<OrderItem> getItens() {
+        return itens;
+    }
 
-	@Override
-	public String toString() {
-		return "OrderSale [id=" + id + ", itens=" + itens + ", discounts=" + discounts + "]";
-	}
+    public void setItens(List<OrderItem> itens) {
+        this.itens = itens;
+    }
 
-	public BigDecimal getTotalPriceItens() {
-		double prince = 0;
-		for(OrderItem i : itens) {
-			prince += i.getPriceSale().doubleValue();
-		}
-		return BigDecimal.valueOf(prince);
-	}
-	
+    public List<OrderDiscount> getDiscounts() {
+        return discounts;
+    }
+
+    public void setDiscounts(List<OrderDiscount> discounts) {
+        this.discounts = discounts;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((itens == null) ? 0 : itens.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OrderSale other = (OrderSale) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (itens == null) {
+            if (other.itens != null)
+                return false;
+        } else if (!itens.equals(other.itens))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderSale [id=" + id + ", itens=" + itens + ", discounts=" + discounts + "]";
+    }
+
+    public BigDecimal getTotalPriceItens() {
+        final Double price = this.itens.stream().mapToDouble(e -> e.getPriceSale().doubleValue()).sum();
+        return BigDecimal.valueOf(price);
+    }
+
 }
