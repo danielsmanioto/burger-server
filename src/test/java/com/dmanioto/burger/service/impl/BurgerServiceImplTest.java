7a36eb352f9d6e96d4bf7d180eb79e4f88a32d87
Daniel Smanioto <daniel.smanioto@gmail.com>
2019-09-11 22:52:09 -0300

    betting test coverage of burger services

--- src/test/java/com/dmanioto/burger/service/impl/BurgerServiceImplTest.java
+++ src/test/java/com/dmanioto/burger/service/impl/BurgerServiceImplTest.java
@@ -1,8 +1,10 @@
 package com.dmanioto.burger.service.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,32 +18,46 @@ import com.dmanioto.burger.service.BurgerService;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BurgerServiceImplTest {
-	
-	@Autowired
-	private BurgerService burgerServiceImpl;
-	
-	@Test
-	public void checkPriceBacon() {
-		Burger xbacon = burgerServiceImpl.getXBacon();
-		assertEquals(BigDecimal.valueOf(6.50).setScale(2), xbacon.getPriceTotal());
-	}
-	
-	@Test
-	public void checkPriceBurger() {
-		Burger xburger = burgerServiceImpl.getXBurger();
-		assertEquals(BigDecimal.valueOf(4.50).setScale(2), xburger.getPriceTotal());
-	}
 
-	@Test
-	public void checkPricexEggBurger() {
-		Burger xeggbacon = burgerServiceImpl.getXEggBacon();
-		assertEquals(BigDecimal.valueOf(7.30).setScale(2), xeggbacon.getPriceTotal());
-	}
-	
-	@Test
-	public void checkPricexEgg() {
-		Burger xegg = burgerServiceImpl.getXEgg();
-		assertEquals(BigDecimal.valueOf(5.30).setScale(2), xegg.getPriceTotal());
-	}
-	
+    @Autowired
+    private BurgerService burgerServiceImpl;
+
+    @Test
+    public void checkPriceBacon() {
+        Burger xbacon = burgerServiceImpl.getXBacon();
+        assertEquals(BigDecimal.valueOf(6.50).setScale(2), xbacon.getPriceTotal());
+    }
+
+    @Test
+    public void checkPriceBurger() {
+        Burger xburger = burgerServiceImpl.getXBurger();
+        assertEquals(BigDecimal.valueOf(4.50).setScale(2), xburger.getPriceTotal());
+    }
+
+    @Test
+    public void checkPricexEggBurger() {
+        Burger xeggbacon = burgerServiceImpl.getXEggBacon();
+        assertEquals(BigDecimal.valueOf(7.30).setScale(2), xeggbacon.getPriceTotal());
+    }
+
+    @Test
+    public void checkPricexEgg() {
+        Burger xegg = burgerServiceImpl.getXEgg();
+        assertEquals(BigDecimal.valueOf(5.30).setScale(2), xegg.getPriceTotal());
+    }
+
+    @Test
+    public void findAll() {
+        List<Burger> burgers = burgerServiceImpl.getAll();
+        assertNotNull(burgers);
+    }
+
+    @Test
+    public void findById() {
+        Burger burger = burgerServiceImpl.getById(1L);
+        assertNotNull(burger);
+        assertEquals(1L, burger.getId(), 0.5);
+        assertEquals("X-Bacon", burger.getDescription());
+    }
+
 }
